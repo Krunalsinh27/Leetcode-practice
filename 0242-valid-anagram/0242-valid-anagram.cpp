6151d675622b02a3099c022d0c5bbf23aabb4fae
Krunalsinh Parmar <parmarkrunalsinh37@gmail.com>
2026-07-02 01:06:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
+      if(s.length() != t.length()){
+        return false;
+      }
+
+      vector<int>arr(26,0);
+
+      for(int i=0; i<s.length(); i++){
+        arr[s[i]-'a']++;
+        arr[t[i]-'a']--;
+      }
+
+      for(int count : arr)
+      {
+        if(count != 0){
             return false;
         }
-
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        return s == t;
+      }
+      return true;
     }
 };
